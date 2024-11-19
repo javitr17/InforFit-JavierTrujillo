e3699865d9780c73d2fdd2979dde66920a3a2398
Tu Nombre <jtruram122@iesmartinezm.es>
2024-11-19 10:19:50 +0100

    commit 18: despliegue prueba

--- InForFit/settings.py
+++ InForFit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 RECAPTCHA_PUBLIC_KEY = '6LcgdXEqAAAAAFvdpPG1TU8IeihXuwHZf1mUCfIj'
 RECAPTCHA_PRIVATE_KEY = '6LcgdXEqAAAAAEZNZdxw-pKJqpwFR9U1DDIcsxfA'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'app/static')
+]
